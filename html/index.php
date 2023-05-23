fc0ae4ef3505c48d6b0d82666cbcb06c1709da50
村上健太 <mgs.snake.riden@gmail.com>
2023-05-23 22:53:24 +0900

    コメント

---
+++ html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>php7.4-apache</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<!-- <?php phpinfo(); ?> -->
+<!-- yumiti -->
+
+</body>
+</html>
