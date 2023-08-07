aaa3c20e27c6732d3bd04c2e7c0a138e3e5e8709
村上健太 <mgs.snake.riden@gmail.com>
2023-08-07 21:32:24 +0900

    出品画面形成

--- src/laravelapp/resources/views/products/create.blade.php
+++ src/laravelapp/resources/views/products/create.blade.php
@@ -1,77 +1,110 @@
 @extends('layouts.app')
 
 @section('content')
-    <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('出品') }}</div>
+                    <div class="card-body">
+                        <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
 
-        <div>
-            <label for="name">商品名</label>
-            <input type="text" name="name" id="name" required>
-            @error('name')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
+                            <div class="row mb-3">
+                                <label for="name"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('商品名') }}</label>
 
-        <div>
-            <label for="description">商品説明</label>
-            <textarea name="description" id="description" required></textarea>
-            @error('description')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
+                                <div class="col-md-6">
+                                    <input type="text" name="name" id="name" required>
+                                    @error('name')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
 
-        <div>
-            <label for="price">商品価格</label>
-            <input type="text" name="price" id="price" required>
-            @error('price')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
+                            <div class="row mb-3">
+                                <label for="description"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('商品説明') }}</label>
 
-        <div>
-            <label for="category_id">カテゴリー</label>
-            <select name="category_id" id="category_id" required>
-                <option value="">カテゴリーを選択してください</option>
-                @foreach($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
-                @endforeach
-            </select>
-            @error('category_id')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
+                                <div class="col-md-6">
+                                    <textarea name="description" id="description" required></textarea>
+                                    @error('description')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
 
-        <div>
-            <label for="tags">タグ</label>
-            <input type="text" name="tags[]" id="tags" placeholder="タグを入力してください">
-            @error('tags')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-        
-        <div class="form-group">
-            <label for="image">画像</label>
-            <input type="file" name="image" id="image" class="form-control-file" accept="image/*" required>
-        </div>
+                            <div class="row mb-3">
+                                <label for="description"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('商品説明') }}</label>
+                                <div class="col-md-6">
+                                    <textarea name="description" id="description" required></textarea>
+                                    @error('description')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
 
-        <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+                            <div class="row mb-3">
+                                <label for="category_id"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('カテゴリー') }}</label>
+                                <div class="col-md-6">
+                                    <select name="category_id" id="category_id" required>
+                                        <option value="">カテゴリーを選択してください</option>
+                                        @foreach ($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    @error('category_id')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="tags"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('タグ') }}</label>
+                                <div class="col-md-6">
+                                    <input type="text" name="tags[]" id="tags" placeholder="タグを入力してください">
+                                    @error('tags')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="image"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('画像') }}</label>
+                                <div class="col-md-6">
+                                    <input type="file" name="image" id="image" class="form-control-file"
+                                        accept="image/*" required>
+                                </div>
+                            </div>
+
+                            <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
 
 
-        @if ($errors->any())
-            <div class="alert alert-danger">
-              <ul>
-               @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-               @endforeach
-              </ul>
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+
+                            <div class="row mb-3">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('商品を出品する') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-        @endif
-
-
-        <!-- 他の商品情報の入力フォームを追加 -->
-
-        <div>
-            <button type="submit">商品を出品する</button>
         </div>
-    </form>
+    </div>
 @endsection
