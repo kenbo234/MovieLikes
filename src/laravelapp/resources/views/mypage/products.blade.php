8b0620e49455aa121bb845fb96e85ce37cff38a4
村上健太 <mgs.snake.riden@gmail.com>
2023-08-07 23:00:33 +0900

    出品した商品画面形成

--- src/laravelapp/resources/views/mypage/products.blade.php
+++ src/laravelapp/resources/views/mypage/products.blade.php
@@ -1,23 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>出品した商品一覧</h1>
-        <ul>
-            @foreach ($products as $product)
-                <li>
-                    {{ $product->name }} - {{ $product->description }} - 価格: {{ $product->price }}
-                    <form action="{{ route('mypage.product.cancel', ['id' => $product->id]) }}" method="POST">
-                        @csrf
-                        <button type="submit" class="btn btn-danger">出品取り消し</button>
-                    </form>
-                </li>
-            @endforeach
-        </ul>
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
+    <div class="album py-5 bg-light">
+        <div class="container">
+            <div class="row">
+                @foreach ($products as $product)
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            @if ($product->images->isNotEmpty())
+                                <img src="{{ asset('storage/' . $product->images->first()->image_url) }}" alt="商品画像">
+                            @else
+                                <img src="{{ asset('images/default_product_image.jpg') }}" alt="デフォルト商品画像">
+                            @endif
+                            <div class="card-body">
+                                <p class="card-text">{{ $product->name }}</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <a href="{{ route('products.show', ['id' => $product->id]) }}"
+                                            class="btn btn-sm btn-outline-secondary">詳細を見る</a>
+                                        <form action="{{ route('mypage.product.cancel', ['id' => $product->id]) }}"
+                                            method="POST">
+                                            @csrf
+                                            <button type="submit" class="btn btn-sm btn-outline-danger">出品取り消し</button>
+                                        </form>
+                                    </div>
+                                    <small class="text-muted">価格: {{ $product->price }}</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endif
+        </div>
+    </div>
+
+
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
     </div>
 @endsection
