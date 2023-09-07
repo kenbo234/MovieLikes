50f2f99ecfe092dc3fa2a506fc487e8101c24e67
村上健太 <mgs.snake.riden@gmail.com>
2023-09-07 10:48:15 +0900

    AppServiceProvider

--- src/laravelapp/app/Providers/AppServiceProvider.php
+++ src/laravelapp/app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +29,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        Schema::defaultStringLength(191);
+
+        if (App::environment('production','staging')) {
+            URL::forceScheme('https');
+        }
     }
 }
